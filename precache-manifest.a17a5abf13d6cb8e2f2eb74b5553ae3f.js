c377185267584d1e3bd0c7a165958c4779f205d9
TamiresCass <tamirescassiano8@gmail.com>
2019-06-21 18:11:40 -0300

    Updates

---
+++ precache-manifest.a17a5abf13d6cb8e2f2eb74b5553ae3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b6362a70618e063d20180ff09c09043",
+    "url": "TamiresCass.github.io/portifolio/index.html"
+  },
+  {
+    "revision": "6e967285e42f66745d95",
+    "url": "TamiresCass.github.io/portifolio/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "24a0ba532fbe3814b08f",
+    "url": "TamiresCass.github.io/portifolio/static/css/main.b5a97272.chunk.css"
+  },
+  {
+    "revision": "6e967285e42f66745d95",
+    "url": "TamiresCass.github.io/portifolio/static/js/2.b06253a0.chunk.js"
+  },
+  {
+    "revision": "24a0ba532fbe3814b08f",
+    "url": "TamiresCass.github.io/portifolio/static/js/main.6e6c3614.chunk.js"
+  },
+  {
+    "revision": "9661f81168814574823e",
+    "url": "TamiresCass.github.io/portifolio/static/js/runtime~main.924f0435.js"
+  },
+  {
+    "revision": "4373b67048b5d74594341d65fc71d535",
+    "url": "TamiresCass.github.io/portifolio/static/media/cv.4373b670.pdf"
+  },
+  {
+    "revision": "4b6915a4f3b61283062749dc28a49646",
+    "url": "TamiresCass.github.io/portifolio/static/media/foto.4b6915a4.jpg"
+  }
+]);
